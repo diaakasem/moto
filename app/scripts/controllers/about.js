6c2d48ec73774260a1996642f86b88696931a344
Diaa Kasem <me@diaa.me>
2016-10-09 14:22:06 +0200

    Initial structure

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name motoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the motoApp
+ */
+angular.module('motoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
